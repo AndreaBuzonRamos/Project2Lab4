6542a908cb1072ea9b542ec0ea1f89523781ba51
AndreaBuzonRamos <157746658+AndreaBuzonRamos@users.noreply.github.com>
2024-03-28 14:11:52 -0400

    I tryed to add facebook like but its not working

--- src/layouts/default/Footer.vue
+++ src/layouts/default/Footer.vue
@@ -1,40 +1,27 @@
 <template>
     <v-footer :app="!mobile" class="footer" elevation="2">
-        <!-- <v-bottom-navigation class="d-block d-sm-none">
-            <v-btn value="recent">
-                <v-icon>mdi-history</v-icon>
-
-                <span>Recent</span>
-            </v-btn>
-
-            <v-btn value="favorites">
-                <v-icon>mdi-heart</v-icon>
-
-                <span>Favorites</span>
-            </v-btn>
-
-            <v-btn value="nearby">
-                <v-icon>mdi-map-marker</v-icon>
-
-                <span>Nearby</span>
-            </v-btn>
-        </v-bottom-navigation> -->
-        <v-content>
-            Antoine Moevus 2024-2054.
-        </v-content>
+      <!-- Contenido del footer -->
+      <v-content>
+        Andrea & Osmany 2024
+  
+        <!-- Botón de "Me gusta" de Facebook -->
+        <div id="fb-root"></div>
+        <div class="fb-like" data-href="" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="false"></div>
+      </v-content>
     </v-footer>
-</template>
-
-<script setup>
-import { useDisplay } from 'vuetify';
-
-const {mobile} = useDisplay()
-</script>
-
-<style scoped>
-.footer {
+  </template>
+  
+  <script setup>
+  import { useDisplay } from 'vuetify';
+  
+  const { mobile } = useDisplay();
+  </script>
+  
+  <style scoped>
+  .footer {
     text-align: center;
     background-color: #4f0780;
     color: white;
-}
-</style>
+  }
+  </style>
+  
