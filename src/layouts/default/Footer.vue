62f061411c7e57c0406876ba61268b63b1f5cc68
AndreaBuzonRamos <157746658+AndreaBuzonRamos@users.noreply.github.com>
2024-03-28 14:49:36 -0400

    better designe. add social media logo

--- src/layouts/default/Footer.vue
+++ src/layouts/default/Footer.vue
@@ -1,27 +1,59 @@
 <template>
-    <v-footer :app="!mobile" class="footer" elevation="2">
-      <!-- Contenido del footer -->
-      <v-content>
-        Andrea & Osmany 2024
-  
-        <!-- Botón de "Me gusta" de Facebook -->
-        <div id="fb-root"></div>
-        <div class="fb-like" data-href="" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="false"></div>
-      </v-content>
-    </v-footer>
-  </template>
-  
-  <script setup>
-  import { useDisplay } from 'vuetify';
-  
-  const { mobile } = useDisplay();
-  </script>
-  
-  <style scoped>
-  .footer {
-    text-align: center;
-    background-color: #4f0780;
-    color: white;
-  }
-  </style>
-  
+  <v-footer :app="!mobile" class="footer" elevation="2">
+    <!-- Contenido del footer -->
+    <v-content>
+      <div class="footer-content">
+        <!-- Texto de derechos reservados en la parte izquierda -->
+        <div class="left-content">
+          <p>Tous droits réservés © 2024 Andrea & Osmany</p>
+        </div>
+
+        <!-- Iconos de redes sociales en la parte derecha -->
+        <div class="right-content">
+          <a href="https://facebook.com/miempresa" class="social-icon"><i class="fab fa-facebook"></i></a>
+          <a href="https://twitter.com/miempresa" class="social-icon"><i class="fab fa-twitter"></i></a>
+          <a href="https://instagram.com/miempresa" class="social-icon"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+    </v-content>
+  </v-footer>
+</template>
+
+<script setup>
+import { useDisplay } from 'vuetify';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+const { mobile } = useDisplay();
+</script>
+
+<style scoped>
+.footer {
+  text-align: center;
+  background-color: #4f0780;
+  color: white;
+}
+
+.footer-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.left-content {
+  /* Estilos para la parte izquierda */
+}
+
+.right-content {
+  /* Estilos para la parte derecha */
+}
+
+.social-icon {
+  color: white;
+  margin-left: 10px;
+}
+
+.social-icon:hover {
+  color: #CCCCCC;
+  /* Cambia el color al pasar el mouse */
+}
+</style>
