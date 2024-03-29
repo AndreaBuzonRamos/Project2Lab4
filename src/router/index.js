5e76586be1fc0f1b2e07161fbf70ae40339ec57e
AndreaBuzonRamos <157746658+AndreaBuzonRamos@users.noreply.github.com>
2024-03-29 18:06:35 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -17,16 +17,16 @@ const routes = [
     ],
   },
   {
-    path: '/pokemon',
+    path: '/item',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
         path: '',
-        name: 'PokeShop',
+        name: 'shop',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "pokeshop" */ '@/views/PokeShop.vue'),
+        component: () => import(/* webpackChunkName: "pokeshop" */ '@/views/shop.vue'),
       },
     ],
   },
