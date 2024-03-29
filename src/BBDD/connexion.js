e46fa3053f9a3cfd3191708ad56e2649f6bc69f7
AndreaBuzonRamos <157746658+AndreaBuzonRamos@users.noreply.github.com>
2024-03-28 20:21:45 -0400

    database

---
+++ src/BBDD/connexion.js
@@ -0,0 +1,40 @@
+const sql = require("mssql");
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_SERVER,
+  DB_PORT,
+  DB_DATABASE,
+} = require("./BBDD");
+
+const sqlConfig = {
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  server: DB_SERVER,
+  port: DB_PORT,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true, // Cambia a true para certificados autofirmados
+  },
+};
+
+async function connectAndQuery() {
+  try {
+    await sql.connect(sqlConfig);
+    const result = await sql.query`select * from PRODUIT`;
+    console.log("Connected to PRODUIT");
+    console.log(result);
+  } catch (err) {
+    console.error("Error en la conexión o consulta:", err);
+  } finally {
+    sql.close();
+  }
+}
+
+connectAndQuery();
